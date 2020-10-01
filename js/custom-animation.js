bdcd36170ef6f9fcc0eb5a1b206984af48a752c9
Adnan070 <adnanjutt918@gmail.com>
2020-10-01 05:04:57 -0700

    Some Designing Changes

---
+++ js/custom-animation.js
@@ -0,0 +1,95 @@
+jQuery(window).scroll(function() {	
+							   
+	jQuery('#pagearea .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+	});
+	
+	
+	jQuery('#section1 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+	});
+	
+	jQuery('#section2 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+	});
+	
+	jQuery('#section3 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+	});
+	
+	jQuery('#section4 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+	});
+		
+	
+	jQuery('#section5 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("bounceIn");
+			}
+	});
+	
+	
+	jQuery('#section6 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInDown");
+			}
+	});
+	
+	jQuery('#section7 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+	});
+	
+	jQuery('#section8 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+	});
+	
+	jQuery('#section9 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInLeft");
+			}
+	});
+	
+	jQuery('#section10 .container').each(function(){
+		var imagePos = jQuery(this).offset().top;
+		var topOfWindow = jQuery(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				jQuery(this).addClass("fadeInRight");
+			}
+	});
+	
+		
+});
