dfeeb6f49357eb9b42ba33a07751c2bcd00bb737
Adnan070 <adnanjutt918@gmail.com>
2020-10-02 09:07:32 -0700

    functionality to nav bar

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let toggle=document.getElementsByClassName('toggleMenu')[0];
+toggle.addEventListener('click',()=>{
+    let displaySite=document.getElementsByClassName('sitenav')[0];
+    if(displaySite.style.display=='block'){
+        displaySite.style.display="none";
+    }
+    else {
+        displaySite.style.display="block";
+    }
+    console.log(displaySite.style.display);
+})
